5e8da6b0bef847d8221bc32aaf2b8b077450c3ad
Doyup Kim <d25kim@uwaterloo.ca>
2024-01-16 15:07:52 -0330

    added more functionalities like delete all button

--- src/components/App.js
+++ src/components/App.js
@@ -4,7 +4,12 @@ const menuItems = [
   { name: "Angus Burger", price: 8.99, category: "burger", count: 0 },
   { name: "Tuna Steak Burger", price: 15.0, category: "burger", count: 0 },
   { name: "Bacon Burger", price: 11.5, category: "burger", count: 0 },
-  { name: "Southwest Chicken Burger", price: 9.99, category: "burger", count: 0 },
+  {
+    name: "Southwest Chicken Burger",
+    price: 9.99,
+    category: "burger",
+    count: 0,
+  },
   { name: "Mozzarella Burger", price: 12.5, category: "burger", count: 0 },
   { name: "Cesar Salad", price: 6.5, category: "salad", count: 0 },
   { name: "BBQ Chicken Salad", price: 13.99, category: "salad", count: 0 },
@@ -15,37 +20,52 @@ const menuItems = [
 ];
 
 function App() {
-
   const [cart, setCart] = useState([]);
+  const [totalPrice, setTotalPrice] = useState(0.0);
+  const checkCartEmpty = cart.length !== 0;
 
   const addToCart = (item) => {
     item.count++;
     if (item.count === 1) {
-      setCart((oldCart) => {
-        return [...oldCart, item]
-      })
+      setCart((oldcart) => {
+        return [...oldcart, item];
+      });
     } else {
       setCart([...cart]);
     }
-  }
+    setTotalPrice(totalPrice + item.price);
+  };
 
-  const clearCart = () => {
-    setCart([]);
-  }
-
-  const deleteItem = (item) => {
+  const handleDelete = (item) => {
     if (item.count === 1) {
       item.count = 0;
-      setCart((oldCart) => {
-        return oldCart.filter((oldItem) => {
-          return oldItem.name !== item.name
-        })
-      })
+      setCart((oldcart) => {
+        return oldcart.filter((oldItem) => {
+          return oldItem.name !== item.name;
+        });
+      });
+      setTotalPrice(totalPrice - item.price);
     } else {
       item.count--;
       setCart([...cart]);
+      setTotalPrice(totalPrice - item.price);
     }
-  }
+  };
+
+  const handleDeleteAll = (item) => {
+    setCart((oldcart) => {
+      return oldcart.filter((olditem) => {
+        return olditem.name !== item.name;
+      });
+    });
+    setTotalPrice(totalPrice - item.price * item.count);
+    item.count = 0;
+  };
+
+  const handleClearCart = () => {
+    setCart([]);
+    setTotalPrice(0.0);
+  };
 
   return (
     <div className="App">
@@ -54,25 +74,29 @@ function App() {
         {menuItems.map((item) => {
           return (
             <li key={item.name}>
-              {item.name} - ${item.price} - {item.category}
+              {item.name} - ${item.price}
               <br />
-              <button onClick={() => addToCart(item)}>Add to Cart</button>
+              <button onClick={() => addToCart(item)}>Add</button>
             </li>
           );
         })}
       </ol>
-      
+
       <h2>Cart</h2>
       <ul>
         {cart.map((item) => {
-          return <li key={item.category}>
-            {item.count} - {item.name} - ${item.price} - {item.category}
-            <br />
-            <button onClick={() => deleteItem(item)}>Delete</button>
-          </li>
+          return (
+            <li key={item.name}>
+              {item.count} - {item.name} - ${item.price}
+              <br />
+              <button onClick={() => handleDelete(item)}>Delete</button>
+              <button onClick={() => handleDeleteAll(item)}>Delete All</button>
+            </li>
+          );
         })}
       </ul>
-      <button onClick={clearCart}>Clear Cart</button>
+      {checkCartEmpty && <button onClick={() => handleClearCart()}>Clear Cart</button>}
+      <h3>Total: ${Math.round((totalPrice + Number.EPSILON) * 100) / 100}</h3>
     </div>
   );
 }
